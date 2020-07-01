e0885d49209e069030e23c315a4cab1d77c3289a
rvolynsk <66560296+rvolynsk@users.noreply.github.com>
2020-07-01 13:53:07 -0400

    Add files via upload
    First implementation of health and score commands

---
+++ Command.java
@@ -0,0 +1,259 @@
+/**
+ * Command Class - Objects of type Command represent (parsed) commands that the user has typed
+ * and wants to invoke
+ * @author Richard Volynski
+ * @version 2.6
+ * 1 July 2020
+ */
+
+
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+abstract class Command {
+    abstract String execute();
+}
+
+class TakeCommand extends Command {
+    private String itemName;
+
+    TakeCommand(String itemName) { this.itemName = itemName; }
+
+    String execute() {
+        GameState state = GameState.instance();
+        Room currentRoom = state.getAdventurersCurrentRoom();
+        switch (this.itemName) {
+            case "":
+                return "Take what? (usage: take <item in room>)";
+            case "all":
+                String result = "";
+                ArrayList<Item> currentRoomContents = currentRoom.getContents();
+                if (!currentRoomContents.isEmpty()) {
+                    Iterator<Item> itr = currentRoomContents.iterator();
+                    while (itr.hasNext()) {
+                        Item item = itr.next();
+                        if (state.inventoryWeightLimitReached(item)) {
+                            result += String.format("You tried to take the %s, but your load was too heavy.\n", item);
+                        } else {
+                            state.addToInventory(item);
+                            currentRoom.remove(itr);
+                            result += String.format("Took %s from %s.\n", item, currentRoom.getName());
+                        }
+                    }
+                } else {
+                    result = String.format("There are no items in %s.", currentRoom.getName());
+                }
+                return result;
+            default:
+                Item item = currentRoom.getItemNamed(itemName);
+                if (item != null) {
+                    if (state.inventoryWeightLimitReached(item)) {
+                        return String.format("Your load is too heavy.");
+                    } else {
+                        state.addToInventory(item);
+                        currentRoom.remove(item);
+                        return String.format("Took %s from %s.", item, currentRoom.getName());
+                    }
+                } else {
+                    return String.format("%s not found in %s.", itemName, currentRoom.getName());
+                }
+        }
+    }
+}
+
+class DropCommand extends Command {
+    private String itemName;
+
+    DropCommand(String itemName) {
+        this.itemName = itemName;
+    }
+
+    String execute() {
+        GameState state = GameState.instance();
+        switch(this.itemName) {
+            case "":
+                return "Drop what? (usage: drop <item in inventory>)";
+            case "all":
+                String result = "";
+                ArrayList<Item> inventory = state.getInventory();
+                if (!inventory.isEmpty()) {
+                    Iterator<Item> itr = inventory.iterator();
+                    while (itr.hasNext()) {
+                        Item item = itr.next();
+                        state.removeFromInventory(itr, item);
+                        Room currentRoom = state.getAdventurersCurrentRoom();
+                        currentRoom.add(item);
+                        result += (item + " dropped.\n");
+                    }
+                } else {
+                    result = "You have no items to drop.";
+                }
+                return result;
+            default:
+                try {
+                    Item item = state.getItemFromInventoryNamed(this.itemName);
+                    state.removeFromInventory(item);
+                    Room currentRoom = state.getAdventurersCurrentRoom();
+                    currentRoom.add(item);
+                    return item + " dropped.";
+                }
+                catch (NoItemException e) {
+                    return e.getMessage();
+                }
+        }
+    }
+}
+
+class MovementCommand extends Command {
+    private String dir;
+
+    /**
+     * MovementCommand - For now, this constructor takes a valid move command entered by the user and stores it
+     * as an instance variable
+     * @param dir - user inputted direction, including save
+     */
+    MovementCommand(String dir) {
+        this.dir = dir;
+    }
+
+    /**
+     * execute() - this method executes command and returns the text that should be printed to the user
+     * in response to that command being executed
+     * @return text description where the user is going
+     */
+    String execute() {
+        Room room = GameState.instance().getAdventurersCurrentRoom().leaveBy(dir);
+        GameState.instance().setAdventurersCurrentRoom(room);
+        String execute = GameState.instance().getAdventurersCurrentRoom().describe();
+        return execute;
+    }
+}
+
+class SaveCommand extends Command {
+    private String saveFilename;
+
+    SaveCommand(String saveFilename) {
+        this.saveFilename = saveFilename;
+    }
+
+    String execute() {
+        try {
+            GameState.instance().store(this.saveFilename);
+            return "Game saved successfully.";
+        } catch (IllegalSaveFormatException e) {
+            return e.getMessage();
+        }
+    }
+}
+
+class UnknownCommand extends Command {
+    private String bogusCommand;
+
+    UnknownCommand(String bogusCommand) {
+        this.bogusCommand = bogusCommand;
+    }
+
+    String execute() {
+        return String.format("Sorry, I don't understand '%s'.", bogusCommand);
+    }
+}
+
+class QuitCommand extends Command {
+
+    String execute() {
+        return null;
+    }
+}
+
+class InventoryCommand extends Command {
+
+    InventoryCommand() {
+    }
+
+    String execute() {
+        ArrayList<Item> inventory = GameState.instance().getInventory();
+        if (inventory.isEmpty()) {
+            return "You have no items.";
+        } else {
+            String result = "You are carrying:\n";
+            for (Item item : inventory) {
+                result += ("  " + item + "\n");
+            }
+            return result;
+        }
+    }
+}
+
+class ItemSpecificCommand extends Command {
+    private String verb;
+    private String noun;
+
+    ItemSpecificCommand(String verb, String noun) {
+        this.noun = noun;
+        this.verb = verb;
+    }
+
+    String execute() {
+        try {
+            Item i = GameState.instance().getItemInVicinityNamed(this.noun);
+            if (i.getMessageForVerb(this.verb) != null) {
+                return i.getMessageForVerb(this.verb);
+            } else {
+                return String.format("You cannot '%s' the %s.", verb, i);
+            }
+        } catch (NoItemException e) {
+            return e.getMessage();
+        }
+    }
+}
+
+class LookCommand extends Command {
+
+    LookCommand() {
+    }
+
+    String execute() {
+        GameState.instance().getAdventurersCurrentRoom().setRoomDescriptionNeeded();
+        String execute = GameState.instance().getAdventurersCurrentRoom().describe();
+        return execute;
+    }
+}
+
+class ScoreCommand extends Command {
+
+    ScoreCommand() {
+    }
+
+    String execute() {
+        String scoreMsg  = "";
+        if (GameState.instance().getScoreMsg().containsKey(GameState.instance().getScore())) {
+             scoreMsg = "You have accumulated " + GameState.instance().getScore() + " points. This gives you a rank of "
+                    + GameState.instance().getScoreMsg().get(GameState.instance().getScore()) + ".";
+        }
+        else {
+            scoreMsg = "You have accumulated " + GameState.instance().getScore() + " You don't have a rank assigned.";
+        }
+
+        return scoreMsg;
+    }
+}
+
+class HealthCommand extends Command {
+
+    HealthCommand() {
+    }
+    String execute() {
+        String healthMsg  = "";
+
+        if (GameState.instance().getHealthMsg().containsKey(GameState.instance().getHealth())) {
+            healthMsg = GameState.instance().getHealthMsg().get(GameState.instance().getHealth());
+        }
+        else {
+            healthMsg = "No health info available";
+        }
+        return healthMsg;
+    }
+}
---
+++ Interpreter.java
@@ -0,0 +1,84 @@
+/**
+ * CPSC 240
+ * Team Programming Assignment #1 — "Zork III"
+ * Interpreter Class -  this is the main() class that directs operations. It creates a Dungeon,
+ * initializes the GameState with it, and repeatedly prompts the user for input. Each time the user
+ * inputs a command, it should use the CommandFactory to instantiate a new Command object and execute it.
+ * If the user enters "q", it terminates the program.
+ * @author Richard Volynski
+ * @version 2.6
+ * 1 July 2020
+ */
+
+package com.company;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.*;
+
+public class Interpreter {
+    private String commandEntered;
+
+    /**
+     * Main class - Argument can be either a .sav file or .ZORK file
+     */
+    public static void main(String[] args) throws IllegalSaveFormatException {
+        Scanner stdin = new Scanner(System.in);
+
+
+        String defaultZorkFile = "ZorkIII_Test_File.zork";
+        if (args.length > 0) {
+            defaultZorkFile = args[0];
+        }
+        else {
+            assert true; // essentially do nothing
+        }
+
+        Dungeon dungeon = null;
+        try {
+            if (defaultZorkFile.endsWith(".sav")) {
+                GameState.instance().restore(defaultZorkFile);
+                dungeon = GameState.instance().getDungeon();
+            }
+            else {
+                dungeon = new Dungeon(defaultZorkFile, true);
+                GameState.instance().initialize(dungeon);
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Exception happened: " + e);
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        System.out.println(dungeon.getTitle());
+        System.out.println();
+        System.out.println(dungeon.getEntry().describe());
+
+        String commandEntered = "";
+        while (!commandEntered.equalsIgnoreCase("q")) {
+            System.out.print("> ");
+            commandEntered = stdin.nextLine();
+            Command command;
+
+            if (commandEntered.toLowerCase().equals("save")) {
+                System.out.print("Enter the name of your save file: ");
+                String saveFilename = stdin.nextLine();
+                command = CommandFactory.instance().parse(commandEntered + " " + saveFilename);
+            } else {
+                command = CommandFactory.instance().parse(commandEntered);
+            }
+            if (command != null) {
+                String output = command.execute();
+                if (output == null) {
+                    System.out.println("See you next time!");
+                }
+                else {
+                    System.out.println(output);
+                }
+            }
+        }
+    }
+}
+
