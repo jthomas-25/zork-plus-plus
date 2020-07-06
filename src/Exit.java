99dcba1a8c71b9897741972d2b4894adaa7622b0
Richard Volynski <rick22182@gmail.com>
2020-07-06 16:35:33 +0000

    Add javadoc descriptions for Exit class

--- src/Exit.java
+++ src/Exit.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
 
 /**
- * Exit Class - Each Room object holds on to an ArrayList of the Exits from it, and each Exit holds on to the
- * Room that it leads to. An Exit can also describe itself, which generates and returns a String message.
- * @author Object Oriented Optimists (OOO)
- * @version 2.5
- * 23 June 2020
+ * Exit Class contains the user's current room (Room object), direction (e.g. "w", "e", "s"), and
+ * room destination (Room object), where current direction (user input) leads to.
+ * An exit can also describe itself, which generates and returns a String message which tells the user
+ * which direction they can go and the name of the surrounding room(s). Also, Exit class implements initialization
+ * from Zork file.
+ * @author Richard Volynski (OOO)
+ * @version 2.6
+ * 8 July 2020
  */
 class Exit {
     private String dir;
@@ -13,10 +16,12 @@ class Exit {
     private Room dest;
     
     /**
-     * Exit - this method throws an exception if a line doesn't contain "===" (indicating the current room)
+     * Exit - this constructor initializes itself, reading lines from .zork file using a scanner.
+     * This constructor throws an exception if a line doesn't contain "===" (indicating end of exits in the .zork file).
+     * This constructor also reads Dungeon class by room name.
      * @param s - Scanner
      * @param d - room in Dungeon class
-     * @throws NoExitException
+     * @throws NoExitException when end of exits reached
      */
     Exit(Scanner s, Dungeon d) throws NoExitException  {
         String line = s.nextLine();
@@ -41,7 +46,7 @@ class Exit {
      * @param src - room where the user is coming room
      * @param dest - room the user is going into, using input direction
      */
-    Exit(String dir, Room src, Room dest) throws NoExitException {
+    Exit(String dir, Room src, Room dest) {
         this.dir = dir;
         this.src = src;
         this.dest = dest;
@@ -49,33 +54,34 @@ class Exit {
 
 
     /**
-     * getDir - this method returns the direction of the room that the user moves towards
-     * @return dir
+     * getDir - this method returns the direction from the current room to the destination room
+     * @return direction
      */
     String getDir() {
         return dir;
     }
 
     /**
-     * getSrc - this method returns the room source
-     * @return src
+     * getSrc - this method returns the current room
+     * @return current room
      */
     Room getSrc() {
         return src;
     }
 
     /**
-     * getDest - this method returns (takes the user into) the room based on the direction that the user entered
-     * @return dest - next room
+     * getDest - this method returns (takes the user into) the destination room based on the
+     * direction that the user entered
+     * @return destination room
      */
     Room getDest() {
         return dest;
     }
 
     /**
-     * describe() - this method returns the text about which rooms the user can enter from the current room
-     * and guides the user which direction to go
-     * @return Room description
+     * describe() - this method returns the text (user-friendly) about which rooms the user can enter from
+     * the current room and guides the user which direction to go
+     * @return exit description
      */
     String describe() {
         return "You can go " + dir + " to " + dest.getName();
@@ -83,7 +89,7 @@ class Exit {
 }
 
 /**
- * class NoExitException is a custom exception
+ * class NoExitException is a custom exception, which is thrown when there are no more exits available
  */
 class NoExitException extends Exception {
 
