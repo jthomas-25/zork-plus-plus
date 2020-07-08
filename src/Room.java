3965c95cebee3979b1056f044f66fdf54291bf08
Richard Volynski <rick22182@gmail.com>
2020-07-08 16:31:53 +0000

    Add Javadoc comments to Room and ZorkEvent classes

--- src/Room.java
+++ src/Room.java
@@ -8,11 +8,13 @@ import java.util.Iterator;
 
 
 /**
- * Room Class - represents every room in the dungeon (name, description), knows whether or not
- * the adventurer has already visited it. Also, the Room Class contains lists of Exits.
+ * Room Class - represents every room in the dungeon.
+ * Room has name and description and stores whether or not the adventurer has already visited it.
+ * Also, the Room Class contains lists of Exits.
  * @author Object Oriented Optimists (OOO)
- * @version 2.5
- * 23 June 2020
+ * @author Richard Volynski
+ * @version 2.7
+ * 8 July 2020
  */
 public class Room {
     private Hashtable <String, Exit> exits;
@@ -22,10 +24,33 @@ public class Room {
     private boolean beenHere;
     private ArrayList<Item> contents;
 
-
+    /**
+     * Currently not used
+     * Room - this constructor initializes itself, reading lines from .zork file using a scanner, which is provided
+     * as a parameter. It also uses Dungeon, provided as a parameter, to find an item by item name, and calls another
+     * Room constructor with three parameters, providing third parameter (initState) as true.
+     * This constructor throws an exception (NoRoomException) if a line does
+     * contain "===" (indicating end of rooms in the .zork file).
+     * @param s - Scanner for .zork file
+     * @param d - reference to Dungeon class
+     * @throws NoRoomException - if a line contains "===" (indicating end of rooms in the zork file).
+     */
     Room(Scanner s, Dungeon d) throws NoRoomException {
         this(s, d, true);
     }
+
+
+    /**
+     * Room - this constructor initializes itself, reading lines from .zork file using a scanner, which is provided
+     * as a parameter. It also uses Dungeon, provided as a parameter, to find an item by item name. The third
+     * parameter (initState) identifies whether to initialize Room content
+     * This constructor throws an exception (NoRoomException) if a line does
+     * contain "===" (indicating end of rooms in the .zork file).
+     * @param s - Scanner for .zork file
+     * @param d - reference to Dungeon class
+     * @param initState - flag to initalize room contents
+     * @throws NoRoomException - if a line contains "===" (indicating end of rooms in the zork file).
+     */
     public Room(Scanner s, Dungeon d, boolean initState) throws NoRoomException {
         init();
         String line = s.nextLine();
@@ -58,8 +83,13 @@ public class Room {
     }
 
     /**
-     * Room - constructor
-     * @param s - Scanner
+     * Currently obsolete
+     * Room - this constructor initializes itself, reading lines from .zork file using a scanner, which is provided
+     * as a parameter.
+     * This constructor throws an exception (NoRoomException) if a line
+     * contains "===" (indicating end of rooms in the .zork file).
+     * @param s - Scanner for .zork file
+     * @throws NoRoomException - if a line contains "===" (indicating end of rooms in the zork file).
      */
     public Room (Scanner s) throws NoRoomException {
         init();
@@ -73,14 +103,19 @@ public class Room {
     }
 
     /**
+     * Currently obsolete
      * Constructor Room - which stores room name in instance variable name
-     * @param name
+     * @param name - Room name
      */
     Room (String name) throws NoExitException {
         init();
         this.name = name;
     }
 
+    /**
+     * init - private method to initalize instance variables for the room, including exits, roomDescriptionNeeded
+     * flag, beenHere flag, and list of contents (items)
+     */
     private void init() {
         exits = new Hashtable<>();
         roomDescriptionNeeded = false;
@@ -90,23 +125,24 @@ public class Room {
 
     /**
      * getName - this method returns name of the room
-     * @return room name
+     * @return name - room name
      */
     String getName() {
         return name;
     }
 
     /**
+     * Currently not used
      * setDesc - this method sets room description
-     * @param desc
+     * @param desc - room description
      */
     void setDesc(String desc) {
         this.desc = desc;
     }
 
     /**
-     * describe - this method generates room description
-     * @return output
+     * describe - this method generates user-friendly room description
+     * @return user-friendly room description
      */
     String describe() {
         String output = "";
@@ -134,14 +170,13 @@ public class Room {
         for (Item i : contents) {
             output = output + "\n" + String.format("There is a(n) %s here.", i);
         }
-
         return output;
     }
 
     /**
-     * leaveBy - this method returns the room from the direction the user is going
-     * @param dir
-     * @return room
+     * leaveBy - this method returns the Room object from the direction the user is going from the current room
+     * @param dir - direction
+     * @return room the user is going to
      */
     Room leaveBy(String dir) {
         if (exits.get(dir.toLowerCase()) == null) {
@@ -154,7 +189,7 @@ public class Room {
     }
 
     /**
-     * addExit - this method stores exit in class Room
+     * addExit - this method adds an exit to the hashtable of the exits in the room
      * @param exit - exit
      */
     public void addExit (Exit exit) {
@@ -162,8 +197,9 @@ public class Room {
     }
 
     /**
-     * storeState - this method stores the state of the room in the file, whether it was visited or not
-     * @param w - PrintWriter to write to file
+     * storeState - this method stores the state of the room in the .sav file, whether the room was visited or not
+     * as well as room contents (items)
+     * @param w - PrintWriter to write to .sav file
      */
     void storeState(PrintWriter w) {
         w.write(getName() + ":\n");
@@ -184,13 +220,20 @@ public class Room {
 
     /**
      * restoreState - this method restores the state of the room from the file, whether it was visited or not
-     * @param r - PrintWriter to write to file
+     * @param r - Scanner to read from .sav file
      */
     void restoreState(Scanner r) {
         String beenHereLine = r.nextLine();
         beenHere = Boolean.parseBoolean(beenHereLine.split("=")[1]);
     }
 
+    /**
+     * restoreState - this method first calls restoreState with Scanner parameter to restore
+     * room's beenHere flag. Then, this method restores room contents (items) and adds them to the list of items
+     * in the room.
+     * @param s - Scanner to read from .sav file
+     * @param d - current Dungeon
+     */
     void restoreState(Scanner s, Dungeon d) {
         restoreState(s);
         String line = s.nextLine();
@@ -205,22 +248,37 @@ public class Room {
         }
     }
 
+    /**
+     * add - this method adds an item to the Room's list of items
+     * @param item - item to add
+     */
     void add(Item item) {
         this.contents.add(item);
     }
 
+    /**
+     * remove - this method removes an item from the Room's list of items
+     * @param item - item to remove
+     */
     void remove(Item item) {
         this.contents.remove(item);
     }
+
     /**
-     * This method will actually let you remove multiple items while iterating
-     * over them, thereby avoiding a ConcurrentModificationException.
-     * @param itr the iterator which will do the removing.
+     * remove - This method removes multiple items while iterating over them.
+     * @param itr - the iterator which will do the removing
      */
     void remove(Iterator<Item> itr) {
         itr.remove();
     }
 
+    /**
+     * getItemNamed - this method checks if user inputted item matches the name
+     * of an item in the user's current room and returns the item if found.
+     * Otherwise, return null.
+     * @param name - item name (user input)
+     * @return item found in the room
+     */
     Item getItemNamed(String name) {
         for (Item item : contents) {
             if (item.goesBy(name)) {
@@ -230,18 +288,25 @@ public class Room {
         return null;
     }
 
+    /**
+     * getContents - this method returns a list of contents (items) from a room
+     * @return room contents (items)
+     */
     ArrayList<Item> getContents() {
         return this.contents;
     }
 
     /**
-     * isBeenHere - this method determines whether or not the user visited the room
-     * @return beenHere
+     * isBeenHere - this method determines whether or not the user visited a room
+     * @return beenHere flag
      */
     public boolean isBeenHere() {
         return beenHere;
     }
 
+    /**
+     * setRoomDescriptionNeeded - this method sets description needed flag for a room
+     */
     public void setRoomDescriptionNeeded() {
         this.roomDescriptionNeeded = true;
     }
--- src/ZorkEvent.java
+++ src/ZorkEvent.java
@@ -1,25 +1,35 @@
 /**
- * ZorkEvent class
+ * ZorkEvent class - An abstract class as the base class for all sub classes for Zork+.
+ * @author Object Oriented Optimists (OOO)
  * @author John Thomas
- * July 2020
+ * @author Richard Volynski
+ * @version 1.1
+ * 8 July 2020
  */
 abstract class ZorkEvent {
+
+    /**
+     * trigger - this is an abstract method that will be implemented at the subclass level
+     * @return event return code
+     */
     abstract String trigger();
-    //abstract void trigger();
 }
 
 /**
- * A ScoreEvent represents a ZorkEvent that, when triggered, changes the player's score by a positive number of points.
- * 
- * Note that while the {@link GameState#setScore} method can also affect the player's score, a ScoreEvent will only increase it.
+ * ScoreEvent - this class extends ZorkEvent class, when triggered, changes the player's score
+ * by a positive number of points.
+ * Note that while the {@link GameState#setScore} method can also affect the player's score,
+ * a ScoreEvent will only increase it.
+ * @author Object Oriented Optimists (OOO)
  * @author John Thomas
+ * @version 1.0
+ * 8 July 2020
  */
 class ScoreEvent extends ZorkEvent {
     private int points;
 
     /**
-     * Constructs a new ScoreEvent with the given number of points.
-     * 
+     * ScoreEvent - Constructor to set a given number of points.
      * @param points the number of points to be added to the player's score
      * @throws IllegalArgumentException if the number of points is less than or equal to 0
      */
@@ -27,35 +37,40 @@ class ScoreEvent extends ZorkEvent {
     }
 
     /**
-     * Adds this event's number of points to the player's score.
-     * @return "" (the empty string) 
+     * trigger - this method implements abstract method from the base class.
+     * It adds this event's number of points to the player's score.
+     * @return event message
      */
     String trigger() {
-        return "";
+        return null;    //TODO implement
     }
 }
 
 /**
- * A WoundEvent represents a ZorkEvent that, when triggered, changes the player's health by a nonzero number of points.
- * Note that a negative number of points will effectively heal the player.
+ * WoundEvent - this class extends ZorkEvent class, changes the player's health
+ * by a nonzero number of points. Note that a negative number of points will
+ * effectively heal the player.
+ * @author Object Oriented Optimists (OOO)
  * @author John Thomas
+ * @author Richard Volynski
+ * @version 1.1
+ * 8 July 2020
  */
 class WoundEvent extends ZorkEvent {
     private int points;
 
     /**
-     * Constructs a new WoundEvent with the given number of points.
-     * 
-     * @param points the number of points to be added to the player's health
-     * @throws IllegalArgumentException if the number of points is equal to 0
+     * WoundEvent - this constructor implements a wound event and awards a given number of points.
+     * @param points - the number of points to be added to the player's health
+     * @throws IllegalArgumentException if the number of points is less than or equal to 0
      */
     WoundEvent(int points) throws IllegalArgumentException {
     }
 
     /**
-     * Adds this event's number of points to the player's health.
+     * trigger - this method implements abstract method from the base class and adds
+     * this event's number of points to the player's health.
      * If the number of points is negative, it will be converted to a positive number before the addition.
-     * 
      * @return a non-numeric message indicating the player's physical condition
      */
     String trigger() {
@@ -64,19 +79,26 @@ class WoundEvent extends ZorkEvent {
 }
 
 /**
- * A DieEvent represents a ZorkEvent that, when triggered, ends the game with the player defeated.
+ * DieEvent - this class extends ZorkEvent class and, when triggered, ends the game with the player defeated.
+ * @author Object Oriented Optimists (OOO)
  * @author John Thomas
+ * @author Richard Volynski
+ * @version 1.1
+ * 8 July 2020
  */
 class DieEvent extends ZorkEvent {
 
     /**
-     * Constructs a new DieEvent.
+     * DieEvent - default constructor
      */
     DieEvent() {
+        //TODO implement
     }
 
     /**
-     *
+     * trigger - this method implements abstract method from the base class and ends the game at its current state,
+     * as well as resets user's health
+     * @return String message indicating that the player is dead
      */
     String trigger() {
         return null;    //TODO implement
@@ -84,19 +106,27 @@ class DieEvent extends ZorkEvent {
 }
 
 /**
- * A WinEvent represents a ZorkEvent that, when triggered, ends the game with the player victorious.
+ * A WinEvent extends ZorkEvent class and, when triggered, ends the game with the player victorious.
+ * @author Object Oriented Optimists (OOO)
  * @author John Thomas
+ * @author Richard Volynski
+ * @version 1.1
+ * 8 July 2020
  */
 class WinEvent extends ZorkEvent {
 
     /**
-     * Constructs a new WinEvent.
+     * WinEvent - default constructor
      */
     WinEvent() {
+        //TODO implement
     }
 
     /**
-     *
+     * trigger - this method implements abstract method from the base class and ends the game at its current state.
+     * This method will print a win message and ask the user if they want to start over. If the user types
+     * yes, reset the game. If user types no, end the game without saving user progress.
+     * @return event message
      */
     String trigger() {
         return null;    //TODO implement
@@ -104,15 +134,19 @@ class WinEvent extends ZorkEvent {
 }
 
 /**
- * A DropEvent represents a ZorkEvent that, when triggered, places an item in the current room,
+ * DropEvent - this class extends ZorkEvent and, when triggered, places an item in the current room,
  * mimicking the effect of the player typing a drop command.
+ * @author Object Oriented Optimists (OOO)
  * @author John Thomas
+ * @author Richard Volynski
+ * @version 1.1
+ * 8 July 2020
  */
 class DropEvent extends ZorkEvent {
     private Item item;
 
     /**
-     * Constructs a new DropEvent with the given item.
+     * DropEvent - this constructor constructs a new DropEvent with the given item.
      * @param item the item to be dropped into the current room
      * @throws NoItemException if the item does not exist in the current room or the player's inventory
      */ 
@@ -120,55 +154,73 @@ class DropEvent extends ZorkEvent {
     }
 
     /**
-     * Removes the item from the player's inventory and adds it to the current room, if the item exists.
-     * If the item already exists in the current room, both the room's contents and the player's inventory remain unchanged.
-     * @return "" (the empty string)
+     * trigger - this method implements abstract method from the base class and removes the item
+     * from the player's inventory and adds it to the current room, if the item exists. If the item
+     * already exists in the current room, both the room's contents and the player's inventory
+     * remain unchanged.
+     * @return event message
      */
     String trigger() {
-        return "";
+        return null;    //TODO implement
     }
 }
 
-/** A DisappearEvent represents a ZorkEvent that, when triggered, removes an item from the game entirely:
- * the item will no longer exist in the current room, the player's inventory, or the dungeon.
+/**
+ * DisappearEvent - this class extends ZorkEvent class and, when triggered, removes an item
+ * from the game entirely: the item will no longer exist in the current room, the player's inventory,
+ * or the dungeon.
+ * @author Object Oriented Optimists (OOO)
  * @author John Thomas
+ * @author Richard Volynski
+ * @version 1.1
+ * 8 July 2020
  */
 class DisappearEvent extends ZorkEvent {
     private String name;
 
     /**
-     * Constructs a new DisappearEvent with the given name.
+     * DisappearEvent - this constructor constructs a new DisappearEvent with the given name.
      * @param name the name of the item to be removed from the game
-     * @throws NoItemException if this name does not correspond to any item in the current room, the player's inventory, or the dungeon.
+     * @throws NoItemException if this name does not correspond to any item in the current room,
+     * the player's inventory, or the dungeon.
      */
     DisappearEvent(String name) throws NoItemException {
     }
 
     /**
-     * Removes the item corresponding to this name from either the current room or the player's inventory,
-     * as well as from the dungeon itself. In other words, if the item does not exist in the current room
+     * trigger - this method implements abstract method from the base class and removes the item
+     * corresponding to this name from either the current room or the player's inventory, as well
+     * as from the dungeon itself. In other words, if the item does not exist in the current room
      * or the inventory, it will simply be removed from the dungeon.
-     * @return "" (the empty string)
+     * @return event message
      */
     String trigger() {
-        return "";
+        return null;    //TODO implement
     }
 }
 
 /**
- * A TransformEvent represents a ZorkEvent that, when triggered,
+ * TransformEvent - this class extends ZorkEvent and, when triggered, remove an item from the user's current
+ * room and replace it another item.
+ * @author Object Oriented Optimists (OOO)
  * @author John Thomas
+ * @author Richard Volynski
+ * @version 1.1
+ * 8 July 2020
  */
 class TransformEvent extends ZorkEvent {
 
     /**
-     * Constructs a new TransformEvent.
+     * TransformEvent - default constructor
      */
     TransformEvent() {
+        //TODO implement
     }
 
     /**
-     * 
+     * trigger - implements abstract method from the base class and removes an item from the user's current
+     * room and replace it with another item.
+     * @return event message
      */
     String trigger() {
         return null;    //TODO implement
@@ -176,21 +228,29 @@ class TransformEvent extends ZorkEvent {
 }
 
 /**
- * A TeleportEvent represents a ZorkEvent that, when triggered,
+ * TeleportEvent - this class extends ZorkEvent and, when triggered, move the user to another room,
+ * regardless if the user already visited that room.
+ * @author Object Oriented Optimists (OOO)
  * @author John Thomas
+ * @author Richard Volynki
+ * @version 1.1
+ * 8 July 2020
  */
 class TeleportEvent extends ZorkEvent {
 
     /**
-     * Constructs a new TeleportEvent.
+     * TeleportEvent - default constructor
      */
     TeleportEvent() {
+        //TODO implement
     }
 
     /**
-     * 
+     * trigger - implements abstract method from the base class and moves the user to another room
+     * @return event message
      */
     String trigger() {
         return null;    //TODO implement
     }
 }
+
